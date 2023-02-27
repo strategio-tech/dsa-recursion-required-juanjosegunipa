6a2cea102fca72d768a16223ff441f7ea32141a8
juanjguanipaf@gmail.com <juanjguanipaf@gmail.com>
2023-02-26 22:24:48 -0500

    done

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -4,7 +4,10 @@ import java.util.*;
 
 public class E01 {
     public int getNthFib(int n) {
-        // FIXME
-        return -1;
+       if(n==0 || n==1)
+           return 0;
+       else if(n==2)
+           return 1;
+       return getNthFib(n-1) + getNthFib(n-2);
     }
 }
